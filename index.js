c8fec5437064db8f99746fb4e119d4866cd5bbab
Edward Compton <e-compton@users.noreply.github.com>
2017-04-11 12:08:39 +0100

    Change to Regex
    This changes makes a space after a full stop mandatory before capitalising. Fixes error of urls changing to strange formats:
    e.g. www.google.com => Www.Google.Com.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const regexp = /(:?\.\s?|^)([A-Za-z\u00C0-\u1FFF\u2800-\uFFFD])/gi;
+const regexp = /(:?\.\s|^)([A-Za-z\u00C0-\u1FFF\u2800-\uFFFD])/gi;
 
 export default function capitalizeSentence(input) {
   return input.replace(regexp, match => match.toUpperCase());
